14ca1aafb555748604981a9585e441e8391199b7
Attila Surányi <attila.suranyi@protonmail.com>
2019-06-18 14:34:42 +0200

    player input stored(selected color, row, token)

--- static/js/game.js
+++ static/js/game.js
@@ -1,21 +1,35 @@
-const boardTokens = document.querySelectorAll('.board.token');
-
-const playerTokens = document.querySelectorAll('.player.token');
-
 const board = document.querySelector('#game-board');
 
 const boardRow = document.querySelectorAll('.board-row');
 
+const selectableColors = document.querySelector('#selectable-colors');
+
+let selectedColor;
+
+var playerSteps = [];
+
 board.addEventListener('click', function(event) {
     let target = event.target;
 
-    for (token of boardTokens) {
-        if (target === token) {
-            target.setAttribute('data-selected', 'true')
-        }
-    }
+    let playerStep = {'selectedColor': 0,
+                        'row': 0,
+                        'token': 0};
 
+    if (target.getAttribute('class') === 'board token') {
+        target.classList.add(selectedColor);
+        playerStep['selectedColor'] = selectedColor;
+        playerStep['row'] = target.dataset.row;
+        playerStep['token'] = target.dataset['token'];
+        playerSteps.push(playerStep);
+    }
 });
 
+selectableColors.addEventListener('click', function(event){
+    let target = event.target;
+
+    if (target.getAttribute('class') === 'player token'){
+        selectedColor = target.getAttribute('id');
+    }
+});
 
 
