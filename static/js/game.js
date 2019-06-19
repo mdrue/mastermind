9de028f6e7cc35acfbadd9693b714f762c389639
Attila Surányi <attila.suranyi@protonmail.com>
2019-06-19 09:52:26 +0200

    winning combination of color generated and stored

--- static/js/game.js
+++ static/js/game.js
@@ -6,9 +6,33 @@ const selectableColors = document.querySelector('#selectable-colors');
 
 let selectedColor;
 
+let playerColorChoices = ["player-color-choice-0-0",
+                            "player-color-choice-0-1",
+                            "player-color-choice-0-2",
+                            "player-color-choice-0-3",
+                            "player-color-choice-1-0",
+                            "player-color-choice-1-1",
+                            "player-color-choice-1-2",
+                            "player-color-choice-1-3"];
+
 var playerSteps = [];
 
-let generate
+var generateWinnerCombIndex = function() {
+    let winnerCombIndex = [];
+    for (i=0; i < 4; i++) {
+        winnerCombIndex.push(Math.floor(Math.random() * 8));
+    }
+    return winnerCombIndex;
+};
+
+let getWinnerComb = function(playerColorChoices) {
+    let winnerCombIndex = generateWinnerCombIndex();
+    let winnerComb = [];
+    for (i of winnerCombIndex) {
+        winnerComb.push(playerColorChoices[i]);
+    }
+    return winnerComb;
+};
 
 board.addEventListener('click', function(event) {
     let target = event.target;
@@ -20,13 +44,14 @@ board.addEventListener('click', function(event) {
     if (target.getAttribute('class') === 'board token') {
         target.classList.add(selectedColor);
         playerStep['selectedColor'] = selectedColor;
-        playerStep['row'] = target.dataset.row;
+        playerStep['row'] = target.dataset['row'];
         playerStep['token'] = target.dataset['token'];
         playerSteps.push(playerStep);
     }
+    getWinnerComb(playerColorChoices)
 });
 
-selectableColors.addEventListener('click', function(event){
+selectableColors.addEventListener('click', function(event) {
     let target = event.target;
 
     if (target.getAttribute('class') === 'player token'){
