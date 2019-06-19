fc9ac48210b6a74a5f3f43588a5db24f3d9de961
Attila Surányi <attila.suranyi@protonmail.com>
2019-06-19 14:22:47 +0200

    player choices are evaluated, and the result is given back

--- static/js/game.js
+++ static/js/game.js
@@ -6,6 +6,9 @@ const selectableColors = document.querySelector('#selectable-colors');
 
 let selectedColor;
 
+//this array contains the strings that marks the colors in the HTML and CSS
+//after the player picks a token on the board, the actual token gets this string as a class,
+//so it will be colored through CSS
 let playerColorChoices = ["player-color-choice-0-0",
                             "player-color-choice-0-1",
                             "player-color-choice-0-2",
@@ -17,6 +20,49 @@ let playerColorChoices = ["player-color-choice-0-0",
 
 var playerSteps = [];
 
+//this function gets the necessary player guesses from playerSteps according to which row we need
+let getActualRowColors = function(actualRowNum, playerSteps) {
+    let actualRowColors = [];
+    bottomLimit = (actualRowNum - 2) * 4;
+
+    Array.from({length: 4}, (x,i) => actualRowColors.push(playerSteps[i + bottomLimit]['selectedColor']));
+
+    return actualRowColors
+};
+
+//this function compares the player guesses and winningComb
+//it returns a result array, which contains 1 and/or 2, 1 meaning the guessed color is right but the position is not,
+// and 2 meaning both the color and the position is correct
+let winningCheck = function(actualRowNum, playerSteps, winnerComb) {
+    let playerGuesses = getActualRowColors(actualRowNum, playerSteps);
+
+    const goodPosAndColor = 2;
+    const goodColor = 1;
+
+    let result = [];
+    let restWin = [];
+    let restPlayerGuesses = [];
+
+    for (i=0; i<playerGuesses.length; i++) {
+        if (winnerComb[i] === playerGuesses[i]) {
+            result.push(goodPosAndColor)
+        }
+        else{
+            restWin.push(winnerComb[i]);
+            restPlayerGuesses.push(playerGuesses[i]);
+        }
+    }
+    for (i=0; i<restWin.length; i++){
+        if(restPlayerGuesses.includes(restWin[i])){
+            result.push(goodColor);
+            restWin.pop(restWin[i]);
+            restPlayerGuesses.pop(restWin[i]);
+        }
+    }
+    return result
+};
+
+//this function generates random numbers between 0 and 7
 var generateWinnerCombIndex = function() {
     let winnerCombIndex = [];
     for (i=0; i < 4; i++) {
@@ -25,8 +71,10 @@ var generateWinnerCombIndex = function() {
     return winnerCombIndex;
 };
 
-let actualRow = 1;
+let actualRowNum = 1;
 
+//this function translates the random numbers to 'colors', meaning strings that mark the colors
+//from the playerColorChoices array
 let getWinnerComb = function(playerColorChoices) {
     let winnerCombIndex = generateWinnerCombIndex();
     let winnerComb = [];
@@ -36,33 +84,41 @@ let getWinnerComb = function(playerColorChoices) {
     return winnerComb;
 };
 
+//we generate the winningComb which contains the array of the colors (e.g.:'player-color-choice-0-0')
+//the player has to guess correctly
+let winnerComb = getWinnerComb(playerColorChoices);
+
 board.addEventListener('click', function(event) {
     let target = event.target;
+
+    actualRowNum = Math.floor((playerSteps.length+1) / 4) + 1;
+    let row = document.querySelector(`#row-${actualRowNum}`);
+    let greenMark = document.createElement('img');
+    greenMark.setAttribute('src', 'static/images/Green-Ball-icon.png');
+    row.appendChild(greenMark);
 
     let playerStep = {'selectedColor': 0,
                         'row': 0,
                         'token': 0};
 
-    actualRow = Math.floor((playerSteps.length+1) / 4) + 1;
-    let row = document.querySelector(`#row-${actualRow}`);
-    let greenMark = document.createElement('img');
-    greenMark.setAttribute('src', 'static/images/Green-Ball-icon.png');
-    row.appendChild(greenMark);
-
+    //here we build the playerStep dictionary, which contains the necessary data about what the player
+    //picked as a color, and what token did he place that color
     if (target.getAttribute('class') === 'board token' && playerSteps.length / 4 >= target.dataset.row - 1) {
         target.classList.add(selectedColor);
         playerStep['selectedColor'] = selectedColor;
         playerStep['row'] = target.dataset['row'];
         playerStep['token'] = target.dataset['token'];
-
-        let playerStepRow = [];
-        playerStepRow.push(playerStep);
-        if (playerStep.length === 4) {
-            playerSteps.push(playerStepRow);
-        }
-        console.log(playerSteps)
+        //here we save this player guess to playerSteps
+        playerSteps.push(playerStep);
     }
-    getWinnerComb(playerColorChoices)
+
+    if (playerSteps.length % 4 === 0) {
+
+        //result will be an array, containing 1 or 2, meaning the guessed color is right, but
+        //not on the right position (1), or both the color and position is correct (2)
+        let result = winningCheck(actualRowNum, playerSteps, winnerComb)
+    }
+
 });
 
 selectableColors.addEventListener('click', function(event) {
