8ebb1040ad94c62659557473df6ec6b990494c5f
Lux0n <gyuri1994@gmail.com>
2019-06-20 11:17:39 +0200

    localstorage final

--- static/js/game.js
+++ static/js/game.js
@@ -151,18 +151,6 @@ selectableColors.addEventListener('click', function(event) {
 });
 
 window.addEventListener('load', function () {
-    /*
-    if(localStorage.length === 0){
-        const winnerComb = getWinnerComb(playerColorChoices);
-        const winnerCombStr = JSON.stringify(winnerComb);
-        localStorage.setItem('winnerComb', winnerCombStr);
-    }
-    else{
-        if(localStorage.length > 1){
-            playerSteps = parseSteps();
-
-        }
-    }*/
     let storageLen = localStorage.length;
     switch(storageLen){
         case 0:
@@ -175,7 +163,7 @@ window.addEventListener('load', function () {
             for(let playerStep of playerSteps){
                 let tokenId = `token-${playerStep['row']}-${playerStep['token']}`;
                 let token = document.getElementById(tokenId);
-                token.setAttribute('class', playerStep['selectedColor']);
+                token.setAttribute('class', `board token ${playerStep['selectedColor']}`);
             }
             break;
         default:
